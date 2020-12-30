668684e70bc06702e5a2d84e756bd824954e3344
55310 <76715532+55310@users.noreply.github.com>
2020-12-30 10:40:10 +0900

    2020-12-30

---
+++ Lesson001/src/com/swcodingschool/hello/Quizcondition.java
@@ -0,0 +1,24 @@
+package com.swcodingschool.hello;
+
+public class Quizcondition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int s = 60;
+		if( s >=90 ) {
+			System.out.println("A");			
+		}
+		else if ( s>=80) {
+			System.out.println("B");
+		}
+		else if ( s>=70) {
+			System.out.println("C");
+		}
+		else {
+			System.out.println("Other");
+		} 
+		
+	}
+
+}
