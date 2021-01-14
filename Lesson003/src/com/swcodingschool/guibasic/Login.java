d828b532fb80921fb6846fa187b62e37e8569ffe
55310 <76715532+55310@users.noreply.github.com>
2021-01-14 11:59:07 +0900

    2021-01-14 11:59

--- Lesson003/src/com/swcodingschool/guibasic/Login.java
+++ Lesson003/src/com/swcodingschool/guibasic/Login.java
@@ -11,7 +11,9 @@ import java.sql.SQLException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
@@ -19,8 +21,8 @@ import javax.swing.border.EmptyBorder;
 public class Login extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField txtUserID;
-	private JTextField txtUserPWD;
+	private JTextField txtUserName;
+	private JPasswordField txtUserPWD;
 
 	/**
 	 * Launch the application.
@@ -30,8 +32,7 @@ public class Login extends JFrame {
 			public void run() {
 				try {
 					// 데이터베이스연결
-					if (DBUtil.dbconn == null)
-						DBUtil.DBConnect();
+					if (DBUtil.dbconn == null) DBUtil.DBConnect();
 
 					Login frame = new Login();
 					frame.setVisible(true);
@@ -60,23 +61,18 @@ public class Login extends JFrame {
 		lblNewLabel.setBounds(87, 21, 259, 40);
 		contentPane.add(lblNewLabel);
 
-		JLabel lblNewLabel_1 = new JLabel("UserID");
-		lblNewLabel_1.setBounds(97, 97, 57, 15);
+		JLabel lblNewLabel_1 = new JLabel("User Name");
+		lblNewLabel_1.setBounds(97, 97, 73, 15);
 		contentPane.add(lblNewLabel_1);
 
 		JLabel lblNewLabel_2 = new JLabel("Password");
-		lblNewLabel_2.setBounds(97, 137, 57, 15);
+		lblNewLabel_2.setBounds(97, 129, 57, 15);
 		contentPane.add(lblNewLabel_2);
 
-		txtUserID = new JTextField();
-		txtUserID.setBounds(182, 94, 164, 21);
-		contentPane.add(txtUserID);
-		txtUserID.setColumns(20);
-
-		txtUserPWD = new JTextField();
-		txtUserPWD.setBounds(182, 134, 164, 21);
-		contentPane.add(txtUserPWD);
-		txtUserPWD.setColumns(20);
+		txtUserName = new JTextField();
+		txtUserName.setBounds(182, 94, 164, 23);
+		contentPane.add(txtUserName);
+		txtUserName.setColumns(20);
 
 		JButton btnLogin = new JButton("Login");
 		btnLogin.addActionListener(new ActionListener() {
@@ -85,49 +81,70 @@ public class Login extends JFrame {
 				if (DBUtil.dbconn == null)
 					DBUtil.DBConnect();
 				// 입력된 유저아이디와 비밀번호를 가져온다.
-				String userid = txtUserID.getText();
-				String userpwd = txtUserPWD.getText();
+				String username = txtUserName.getText();
+				String userpwd = new String(txtUserPWD.getPassword());
 				// 입력된 아이디와 비번확인
-				// System.out.println(userid);
-				// System.out.println(userpwd);
-
+//				System.out.println(userid);
+//				System.out.println(userpwd);
+				
 				// 유저아이디와 비밀번호를 사용하여 데이터베이스 조회
-				// SELECT 속성리스트[* 모든 속성 all]
+				// SELECT 속성리스트[* all 모든 속성] 
 				// FROM 테이블이름
 				// WHERE 조건
 				
 				// sql 구문 구성
-				String sql = "SELECT * FROM tbluser WHERE userid=? AND userpwd=?";
-
+				String sql = "SELECT * FROM tbluser WHERE username=? AND userpwd=?";
+				
 				try {
 					// prepared statement는 sql 구문을 좀더 단순하게 구성할 수 있도록 한다.
 					PreparedStatement pstmt = DBUtil.dbconn.prepareStatement(sql);
-					pstmt.setString(1, userid); // userid 변수값으로 sql구문의 첫번째 ? 에 대입
+					pstmt.setString(1, username); // userid 변수값으로 sql구문의 첫번째 ? 에 대입
 					pstmt.setString(2, userpwd);// userpwd 변수값으로 sql구문의 두번째 ? 에 대입
-
+					
 					// 최종 완성된 질의구문을 실행하고 그 결과를 ResultSet으로 받아온다.
 					ResultSet rs = pstmt.executeQuery();
-
+					
 					// resultset rs내에는 검색된 결과값들이 들어있는데...
-					if (rs.next()) { // 해당 계정이 있으면, 정상 로그인
+					if(rs.next()) { // 해당 계정이 있으면, 정상 로그인
 						rs.close();
 						pstmt.close();
 						//System.out.println("로그인성공");
-						// 애플리케이션을 띄워준다
-						// 로그인창을 닫고...
+						// 애플리케이션을 띄워준다.
+						// 로그인창을 닫고....
 						dispose();
-						AppHome apphome = new AppHome(); // 새 프레임을 생성하고
-						apphome.setVisible(true); //프레임을 보이도록 한다
-					} else {// 해당 계정이 없음
-						System.out.println("아이디와 비번을 다시 확인해주세요.");
+						if(username.equals("admin")) {
+							UserManager usermanager = new UserManager();
+							usermanager.setVisible(true);
+						}else {
+							BookInfo apphome = new BookInfo(); // 새 프레임을 생성하고
+							apphome.setVisible(true); // 프레임을 보이도록 한다.
+						}
+					}else {// 해당 계정이 없음
+						//System.out.println("아이디와 비번을 다시 확인해주세요.");
+						JOptionPane.showMessageDialog(null, "아이디와 비번을 다시 확인해주세요.");
 					}
-				} catch (SQLException elogin) {
+				}catch(SQLException elogin) {
 					System.out.println("[MyMSG]SQL Exception Error : " + elogin.getMessage());
 					elogin.printStackTrace();
 				}
 			}
 		});
-		btnLogin.setBounds(249, 180, 97, 23);
+		btnLogin.setBounds(273, 180, 73, 23);
 		contentPane.add(btnLogin);
+
+		txtUserPWD = new JPasswordField();
+		txtUserPWD.setBounds(182, 125, 164, 23);
+		contentPane.add(txtUserPWD);
+		
+		JButton btnJoin = new JButton("Join");
+		btnJoin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();//현재 창을 닫고
+				Join join = new Join();//새 창 객체 생성
+				join.setVisible(true);//새 창 객체를 보이도록 
+			}
+		});
+		btnJoin.setBounds(180, 180, 73, 23);
+		contentPane.add(btnJoin);
 	}
 }
--- Lesson003/src/com/swcodingschool/guibasic/UserManager.java
+++ Lesson003/src/com/swcodingschool/guibasic/UserManager.java
@@ -10,7 +10,7 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class AppHome extends JFrame {
+public class UserManager extends JFrame {
 
 	private JPanel contentPane;
 
@@ -21,7 +21,7 @@ public class AppHome extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					AppHome frame = new AppHome();
+					UserManager frame = new UserManager();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -33,8 +33,8 @@ public class AppHome extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public AppHome() {
-		setTitle("Application Home");
+	public UserManager() {
+		setTitle("User Manager");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -42,16 +42,15 @@ public class AppHome extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNext = new JButton("Next");
-		btnNext.addActionListener(new ActionListener() {
+		JButton btnExit = new JButton("Exit");
+		btnExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// DB가 연결되어진 상태라면... 연결을 종료하고...
-				if(DBUtil.dbconn != null) DBUtil.DBClose();
-				dispose(); // 윈도우 창을 닫음, 창을 화면에서 안보이게끔 감쳐주는 메서드
-				System.exit(0); // 전체 프로그램을 정상적으로 종료
+				dispose();
+				Login login = new Login();
+				login.setVisible(true);
 			}
 		});
-		btnNext.setBounds(299, 211, 97, 23);
-		contentPane.add(btnNext);
+		btnExit.setBounds(325, 228, 97, 23);
+		contentPane.add(btnExit);
 	}
 }
