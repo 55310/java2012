159cf870a137081797f1c060d4bf6b697bc0d18e
55310 <76715532+55310@users.noreply.github.com>
2021-02-01 16:59:39 +0900

    2021-02-01 16:59

---
+++ Lesson242/src/main/java/com/swcodingschool/Lesson242/User.java
@@ -0,0 +1,41 @@
+package com.swcodingschool.Lesson242;
+
+// You need to create the entity model,as following code. 
+// Hibernate automatically translates the entity into a table.
+// 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	
+	private String name;
+	private String email;
+	
+	public Integer getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}//end of class
+
+// now, you can create the Repository like UserRepository.java
