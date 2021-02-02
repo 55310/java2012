52d0a9d4abbb19121d2ec6fd56572cfb9bad75ee
55310 <76715532+55310@users.noreply.github.com>
2021-02-02 16:37:28 +0900

    2021-02-02 16:37

---
+++ Lesson252/src/test/java/com/swcodingschool/Lesson252/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swcodingschool.Lesson252;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
