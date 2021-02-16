9743fd7c4b5301a2f08334663c6cb2282ceb0b2a
55310 <76715532+55310@users.noreply.github.com>
2021-02-16 16:58:25 +0900

    2021-02-16 16:55

---
+++ JPAexample/src/main/java/Example/DDD.java
@@ -0,0 +1,19 @@
+package Example;
+
+public class DDD {
+
+	public static void main(String[] args) {        
+        
+		int num1 = one(4); 
+		System.out.println(num1);
+		} 
+	
+	
+	public static int one(int a) { 
+		return a + a + a; 
+		}
+
+		
+      
+	
+}
